8ad38a0e0c55c0b55989d0507912f8c213846fe1
Jamie Shin <jarwonshin@gmail.com>
2013-10-13 16:07:16 -0400

    reinitialized files

---
+++ portfolio777/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio777::Application.config.secret_key_base = 'c8d263836fa3cfa71b896d54a3caff9f7f5c4419d939e17ab2b2b374e3234b9c6f247cccc5161dc2ca5c1fffe3d66a877396d01e6238fcb0f3d8e8fef9de62ea'
---
+++ portfolio777/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Portfolio777::Application.config.session_store :cookie_store, key: '_portfolio777_session'
